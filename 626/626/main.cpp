af8f42fa0260925177d7c98bc6ebbb849ba013ea
Jiamin LI <jiaminli.icy@gmail.com>
2018-02-28 01:29:41 +0800

    626

--- 626/626/main.cpp
+++ 626/626/main.cpp
@@ -3,14 +3,26 @@
 #include <algorithm>
 using namespace std;
 int m,n;
-long dp(int n){
-    if(n==0) return 0;
-    if(n==1) return 1;
-    
+int b1, b2, b3, b4;
+int dp[1000005];
+int solve(){
+    for(int i=0; i<n; i++){
+        if(i==0) dp[i]=1;
+        else {
+            if(((i+1)%m)!=0){
+                dp[i]=dp[i-1]%1000000007;
+            }
+            else{
+                dp[i]=(dp[i-1]+dp[(i+1)/m-1])%1000000007;
+            }
+        }
+    }
+    return dp[n-1];
 }
 int main(){
-    while(scanf("%d %d",&m,&n)!=EOF){
-        long result=dp(n);
-        cout<<result%1000000007<<endl;
+    while(scanf("%d %d",&n,&m)!=EOF){
+        long arr[4]={1,m,m*m,m*m*m};
+        int res=solve();
+        cout<<res<<endl;
     }
 }
